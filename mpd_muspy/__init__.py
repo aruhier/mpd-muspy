8da537c32585da4e351901478b447723b7d2561e
Anthony Ruhier <anthony.ruhier@gmail.com>
2015-02-28 00:02:14 +0100

    Fix bugs and crashes
    Fix path problems
    Move a big part of the run.py into sync.py, easier to profile

--- mpd_muspy/__init__.py
+++ mpd_muspy/__init__.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python
 # Author: Anthony Ruhier
 
+import __main__
+import os
+import sys
+
+# Check that the configuration exists
+try:
+    _current_dir = os.path.dirname(__main__.__file__)
+except AttributeError:
+    _current_dir = os.getcwd()
+if not os.path.exists(os.path.join(_current_dir, "config.py")):
+    print("Configuration file config.py not found. Please copy the "
+          "config.py.default as config.py.", file=sys.stderr)
+    sys.exit(1)
+
 _version = "0.1a"
 _release_name = "mpd_muspy"
---
+++ mpd_muspy/sync.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+# Author: Anthony Ruhier
+
+import os
+import mpd
+import threading
+from .muspy_api import Muspy_api
+from .artist_db import Artist_db
+from . import _current_dir
+from config import ARTISTS_JSON, SERVER, PORT
+
+ARTISTS_JSON = os.path.join(_current_dir, ARTISTS_JSON)
+
+
+class SyncThread(threading.Thread):
+    """
+    Launch synchronisation with muspy
+    """
+    def __init__(self, artists, artist_db, muspy_api, lock):
+        super().__init__()
+        self.artists = artists
+        self.artist_db = artist_db
+        self.muspy_api = muspy_api
+        self.lock = lock
+
+    def run(self):
+        for artist in self.artists:
+            try:
+                self.muspy_api.add_artist(artist)
+                self.lock.acquire()
+                self.artist_db.mark_as_uploaded(artist)
+                print("Artist:", artist, ". Done...")
+                self.lock.release()
+            except Exception as e:
+                print(e)
+                pass
+            finally:
+                self.lock.acquire()
+                self.artist_db.save()
+                self.lock.release()
+
+
+def chunks(l, n):
+    """
+    Yield successive n-sized chunks from l.
+    """
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
+
+
+def mpd_get_artists(artist_db, mpdclient):
+    mpdclient.connect(SERVER, PORT)
+    try:
+        artists = set(str(artist).lower()
+                      for artist in mpdclient.list("artist") if len(artist))
+    except mpd.ConnectionError:
+        mpdclient.connect(SERVER, PORT)
+        artists = set(str(artist).lower()
+                      for artist in mpdclient.list("artist") if len(artist))
+    return artists
+
+
+def start_threads(non_uploaded_artists, artist_db):
+    lock = threading.Lock()
+    threads = []
+    muspy_api = Muspy_api()
+    for l in chunks(non_uploaded_artists, 3):
+        thread = SyncThread(artists=l, artist_db=artist_db,
+                            muspy_api=muspy_api, lock=lock)
+        thread.daemon = True
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+
+
+def run():
+    artist_db = Artist_db(jsonpath=ARTISTS_JSON)
+    mpdclient = mpd.MPDClient()
+
+    artists = mpd_get_artists(artist_db, mpdclient)
+    artists_removed, artists_added = artist_db.merge(artists)
+    artist_db.save()
+    non_uploaded_artists = artist_db.get_non_uploaded()
+    artists = None
+
+    print(len(non_uploaded_artists), "artist(s) non uploaded on muspy")
+    print(len(artists_added), "artist(s) added")
+    print(len(artists_removed), "artist(s) removed")
+
+    start_threads(non_uploaded_artists, artist_db)
+    print("Done: ",
+          len(non_uploaded_artists) - len(artist_db.get_non_uploaded()),
+          "artist(s) updated")
