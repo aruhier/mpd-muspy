294ef60a6534efbc0ff17ab650f57285b37a5274
Anthony Ruhier <anthony.ruhier@gmail.com>
2017-05-29 19:25:11 +0200

    Release 1.0.1

--- mpd_muspy/__init__.py
+++ mpd_muspy/__init__.py
@@ -1,3 +1,3 @@
 
-_version = "1.0.0"
+_version = "1.0.1"
 _release_name = "mpd-muspy"
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(
     name="mpd-muspy",
-    version="1.0.0",
+    version="1.0.1",
 
     description="Sync artists of your MPD database with your MuSpy account",
 
